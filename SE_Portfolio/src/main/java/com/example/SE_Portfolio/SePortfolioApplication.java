a9fed8097b7f86704ac6934277b758f87e12b893
NiklasNeuweiler <148544728+NiklasNeuweiler@users.noreply.github.com>
2025-04-27 20:54:47 +0200

    Spring init

---
+++ SE_Portfolio/src/main/java/com/example/SE_Portfolio/SePortfolioApplication.java
@@ -0,0 +1,13 @@
+package com.example.SE_Portfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SePortfolioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SePortfolioApplication.class, args);
+	}
+
+}
---
+++ SE_Portfolio/src/test/java/com/example/SE_Portfolio/SePortfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SE_Portfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SePortfolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
