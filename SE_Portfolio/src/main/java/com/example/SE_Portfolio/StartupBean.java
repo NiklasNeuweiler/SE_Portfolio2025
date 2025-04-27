6a20d483b132e390c1c448928aea95359f414817
NiklasNeuweiler <148544728+NiklasNeuweiler@users.noreply.github.com>
2025-04-27 23:31:37 +0200

    Setup

---
+++ SE_Portfolio/src/main/java/com/example/SE_Portfolio/StartupBean.java
@@ -0,0 +1,20 @@
+package com.example.SE_Portfolio;
+
+import jakarta.annotation.PostConstruct;
+import org.openapitools.client.api.DefaultApi;
+import org.openapitools.client.model.GameDto;
+import org.openapitools.client.model.GameInputDto;
+
+public class StartupBean {
+    @PostConstruct
+    public void init() {
+        DefaultApi defaultApi = new DefaultApi();
+
+        GameInputDto gameInput = new GameInputDto();
+        gameInput.setGroupName("Niklas Test");
+
+        GameDto result = defaultApi.gamePost(gameInput);
+        System.out.println("Neues Spiel gestartet: " + result);
+
+    }
+}
